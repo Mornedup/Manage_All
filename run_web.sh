370931c03ba5a01caf8f6d442faf51545dc3cc0e
MDuPreez2 <mdupreez2@gmail.com>
2018-10-24 14:36:29 +0200

    docker fix

--- run_web.sh
+++ run_web.sh
@@ -9,5 +9,5 @@ yes | python manage.py migrate
 
 sleep 5
 
-#gunicorn manage_all.wsgi:application -b 0:8000 -w 6 --log-level DEBUG
+#gunicorn manage_all.wsgi:application -b 0:8001 -w 6 --log-level DEBUG
 python manange.py runserver
