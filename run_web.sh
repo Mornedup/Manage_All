c2a5fb99a759d9c75c64c75729fb4cd760e86413
patricktchankue <ptchankue@gmail.com>
2018-10-24 14:47:01 +0200

    using django web server

--- run_web.sh
+++ run_web.sh
@@ -10,4 +10,4 @@ yes | python manage.py migrate
 sleep 5
 
 #gunicorn manage_all.wsgi:application -b 0:8001 -w 6 --log-level DEBUG
-python manage.py runserver 0.0.0.0:8001
+python manage.py runserver 0.0.0.0:8001 # port
