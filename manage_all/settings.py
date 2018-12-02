1200775d28107ce07bf146ca8f7a3b0fb6459a4b
Morne Du Preez <31358648+Mornedup@users.noreply.github.com>
2018-12-02 18:39:28 +0200

    fixed os enviroment var

--- manage_all/settings.py
+++ manage_all/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+DB_TYPE = os.environ.get('DB_TYPE', 'sqlite')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -76,7 +78,6 @@ WSGI_APPLICATION = 'manage_all.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DB_TYPE = os.environ.get('DB_TYPE', 'sqlite')
 
 if DB_TYPE == 'postgresql':
     DATABASES = {
